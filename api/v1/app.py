84e26292a005ba4a9673f39885b696e44116d1ca
el mehdi lkhaoua <elmehdilkhaoua@gmail.com>
2023-10-28 22:17:27 +0100

    using json pretty print

--- api/v1/app.py
+++ api/v1/app.py
@@ -7,6 +7,7 @@ from api.v1.views import app_views
 
 # app instance of Flask class.
 app = Flask(__name__)
+app.config['JSONIFY_PRETTYPRINT_REGULAR'] = True
 # register app_views blueprint
 app.register_blueprint(app_views)
 
