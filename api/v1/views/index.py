248cdbe60907e0a09cd4211d75f55fe2882e5387
el mehdi lkhaoua <elmehdilkhaoua@gmail.com>
2023-10-28 22:12:12 +0100

    fix styling

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -4,8 +4,7 @@ from api.v1.views import app_views
 from flask import jsonify
 
 
-@app_views.route('/status', methods=['GET'], strict_slashes=False)
-def get_status():
-    return jsonify({
-        'status': 'OK'
-    })
+@app_views.route('/status', strict_slashes=False)
+def status():
+    """ check status of the api. """
+    return jsonify({"status": "OK"})
