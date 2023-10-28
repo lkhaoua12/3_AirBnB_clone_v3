34b0aba0118c60c39b8f7021118b9d3d853b90d5
el mehdi lkhaoua <elmehdilkhaoua@gmail.com>
2023-10-28 22:29:50 +0100

    fixed status

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,24 +1,38 @@
 #!/usr/bin/python3
-""" define routes of the api """
-from flask import Flask
+"""
+This module contains the principal application
+"""
 from models import storage
 from api.v1.views import app_views
+from flask import Flask, make_response, jsonify
 from os import getenv
 
-# app instance of Flask class.
 app = Flask(__name__)
 app.config['JSONIFY_PRETTYPRINT_REGULAR'] = True
-# register app_views blueprint
 app.register_blueprint(app_views)
 
 
 @app.teardown_appcontext
-def tear_down(Exception):
-    """ end the db connection """
+def close_db(obj):
+    """ calls methods close() """
     storage.close()
 
 
+@app.errorhandler(404)
+def page_not_foun(error):
+    """ Loads a custom 404 page not found """
+    return make_response(jsonify({"error": "Not found"}), 404)
+
+
+app.config['SWAGGER'] = {
+    'title': 'AirBnB clone - RESTful API',
+    'description': 'This is the api that was created for the hbnb restful api project,\
+    all the documentation will be shown below',
+    'uiversion': 3}
+
 if __name__ == "__main__":
-    host = getenv("HBNB_API_HOST", "0.0.0.0")
-    port = getenv("HBNB_API_PORT", 5000)
+
+    host = getenv('HBNB_API_HOST', default='0.0.0.0')
+    port = getenv('HBNB_API_PORT', default=5000)
+
     app.run(host, int(port), threaded=True)
